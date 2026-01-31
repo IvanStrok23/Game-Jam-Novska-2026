7c95eeff735008c5ac8502ac5d14b150cd266d25
Ivan Ljutak <matejjelic96@gmail.com>
2026-01-31 20:50:07 +0100

    up

--- Assets/Jelic/StaticWiperController.cs
+++ Assets/Jelic/StaticWiperController.cs
@@ -2,15 +2,49 @@ using UnityEngine;
 
 public class StaticWiperController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    [SerializeField] private Transform fakeCarTransform;
+    [SerializeField] private Transform carTransform;
+
+    [SerializeField] private Transform playerCamera;
+    [SerializeField] private Transform rtCamera;
+
+    [SerializeField] private Transform leftWiper;
+    [SerializeField] private Transform rightWiper;
+
+    [SerializeField] private Transform leftParticle;
+    [SerializeField] private Transform rightParticle;
+
+
+    private void Update()
     {
-        
+        Pose leftWiperPose = GetPose(leftWiper);
+        leftParticle.SetPositionAndRotation(leftWiperPose.position, leftWiperPose.rotation);
+
+        Pose rightWiperPose = GetPose(rightWiper);
+        rightParticle.SetPositionAndRotation(rightWiperPose.position, rightWiperPose.rotation);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void LateUpdate()
     {
-        
+        Pose rtPose = GetPose(playerCamera);
+        rtCamera.SetPositionAndRotation(rtPose.position, rtPose.rotation);
+
     }
+
+    private Pose GetPose(Transform relative)
+    {
+        Vector3 rtPos = carTransform.InverseTransformPoint(relative.position);
+        Quaternion rtRot = Quaternion.Inverse(carTransform.rotation) * relative.rotation;
+
+
+        Pose pose = new Pose()
+        {
+            position = rtPos,
+
+            rotation = rtRot
+        };
+        return pose;
+    }
+
 }
